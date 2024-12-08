fc047d9460e7ee6ccb586baed5ff1a32c222eb5f
Sathish Dusharla <23eg105a16@anurag.edu.in>
2024-12-08 23:33:44 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -14,25 +14,6 @@ import AdminDashboard from './pages/AdminDashboard';
 import ConductElections from './pages/ConductElections';
 import AddAdmin from './pages/AddAdmin'; // Import AddAdmin
 import PrivacyPolicy from './pages/PrivacyPolicy'; // Import PrivacyPolicy
-
-// Admin route protection wrapper
-const ProtectedAdminRoute = ({ children }: { children: JSX.Element }) => {
-  const adminEmail = localStorage.getItem('adminEmail');
-  if (!adminEmail) {
-    return <Navigate to="/admin-login" />;
-  }
-  return children;
-};
-
-// Voter route protection wrapper
-const ProtectedVoterRoute = ({ children }: { children: JSX.Element }) => {
-  const voterEmail = localStorage.getItem('voterEmail');
-  if (!voterEmail) {
-    return <Navigate to="/voter-login" />;
-  }
-  return children;
-};
-
 function App() {
   return (
     <Router>
@@ -42,21 +23,15 @@ function App() {
           <Route path="about" element={<About />} />
           <Route path="contact" element={<Contact />} />
           <Route path="help" element={<Help />} />
-          
-          {/* Voter Routes */}
           <Route path="voter-login" element={<VoterLogin />} />
           <Route path="voter-registration" element={<VoterRegistration />} />
-          <Route path="voting" element={<ProtectedVoterRoute><Voting /></ProtectedVoterRoute>} />
-          <Route path="voter_dashboard" element={<ProtectedVoterRoute><VoterDashboard /></ProtectedVoterRoute>} />
-
-          {/* Admin Routes */}
           <Route path="admin-login" element={<AdminLogin />} />
-          <Route path="admin-dashboard" element={<ProtectedAdminRoute><AdminDashboard /></ProtectedAdminRoute>} />
-          <Route path="admin/conduct-elections" element={<ProtectedAdminRoute><ConductElections /></ProtectedAdminRoute>} />
-          <Route path="admin/add-admin" element={<ProtectedAdminRoute><AddAdmin /></ProtectedAdminRoute>} />
-          <Route path="admin/results" element={<ProtectedAdminRoute><Results /></ProtectedAdminRoute>} />
-
-          {/* Other Pages */}
+          <Route path="voting" element={<Voting />} />
+          <Route path="results" element={<Results />} />
+          <Route path="voter_dashboard" element={<VoterDashboard />} />
+          <Route path="admin-dashboard" element={<AdminDashboard />} />
+          <Route path="admin/conduct-elections" element={<ConductElections />} />
+          <Route path="admin/add-admin" element={<AddAdmin />} />
           <Route path="/privacy-policy" element={<PrivacyPolicy />} />
         </Route>
       </Routes>
